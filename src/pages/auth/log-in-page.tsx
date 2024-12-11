9f4839d6d432734b755611a5de45d43ff74fc1e7
huyngse <ngogiahuy775309@gmail.com>
2024-12-11 18:21:28 +0700

    Log in page

--- src/pages/auth/log-in-page.tsx
+++ src/pages/auth/log-in-page.tsx
@@ -1,7 +1,89 @@
+import { PasswordInput } from "@/components/password-input";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
+import { z } from "zod";
+const formSchema = z.object({
+  email: z.string().email("Email không hợp lệ"),
+  password: z.string().min(5, "Mật khẩu phải chứa ít nhất 5 ký tự"),
+});
 const LoginPage = () => {
+  const navigate = useNavigate();
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+  const handleSubmit = () => {
+    navigate("/");
+  };
   return (
-    <div>LoginPage</div>
-  )
-}
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <div className="w-full max-w-xs">
+        <Form {...form}>
+          <form
+            className="bg-white shadow-md rounded px-8 pt-6 pb-8 space-y-5"
+            onSubmit={form.handleSubmit(handleSubmit)}
+          >
+            <h2 className="text-center text-4xl font-bold">Đăng Nhập</h2>
+            <p className="text-center font-semibold">
+              Innovibe Inventory Management System
+            </p>
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="hidden">Email</FormLabel>
+                  <Input
+                    placeholder="Địa chỉ email"
+                    type="email"
+                    className="py-6"
+                    {...field}
+                  />
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem className="mt-5">
+                  <FormLabel className="hidden">Password</FormLabel>
+                  <PasswordInput
+                    placeholder="Mật khẩu"
+                    className="py-6"
+                    {...field}
+                  />
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <div className="flex items-center justify-center">
+              <Button
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-full"
+                type="submit"
+              >
+                Log in
+              </Button>
+            </div>
+          </form>
+        </Form>
+      </div>
+    </div>
+  );
+};
 
-export default LoginPage
+export default LoginPage;
