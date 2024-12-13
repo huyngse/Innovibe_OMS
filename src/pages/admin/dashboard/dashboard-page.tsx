b967d4d5e4de8ca76ec50df2e512976d6078f2a5
huyngse <ngogiahuy775309@gmail.com>
2024-12-13 08:51:31 +0700

    Dashboard - Overview section

--- src/pages/admin/dashboard/dashboard-page.tsx
+++ src/pages/admin/dashboard/dashboard-page.tsx
@@ -1,7 +1,15 @@
+import Header from "@/components/header";
+import Overview from "./overview";
+
 const DashboardPage = () => {
   return (
-    <div>DashboardPage</div>
-  )
-}
+    <div>
+      <Header title="Tổng quan" href="/" currentPage="Dashboard" />
+      <div className="grid grid-cols-12 gap-5 p-5">
+        <Overview />
+      </div>
+    </div>
+  );
+};
 
-export default DashboardPage
+export default DashboardPage;
