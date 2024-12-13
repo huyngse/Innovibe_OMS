94cd42927cfaaafb41573a446e371f44ab8a753d
huyngse <ngogiahuy775309@gmail.com>
2024-12-13 20:39:27 +0700

    Dashboard - Line chart

--- src/pages/admin/dashboard/dashboard-page.tsx
+++ src/pages/admin/dashboard/dashboard-page.tsx
@@ -1,5 +1,6 @@
 import Header from "@/components/header";
 import Overview from "./overview";
+import Revenue from "./revenue";
 
 const DashboardPage = () => {
   return (
@@ -7,6 +8,7 @@ const DashboardPage = () => {
       <Header title="Tổng quan" href="/" currentPage="Dashboard" />
       <div className="grid grid-cols-12 gap-5 p-5">
         <Overview />
+        <Revenue />
       </div>
     </div>
   );
