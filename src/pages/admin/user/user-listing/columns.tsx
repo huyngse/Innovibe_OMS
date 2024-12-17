b3cfb410708ba31b664fc8c2ecb3cbeabba2a061
huyngse <ngogiahuy775309@gmail.com>
2024-12-17 20:54:41 +0700

    User data table - action column

--- src/pages/admin/user/user-listing/columns.tsx
+++ src/pages/admin/user/user-listing/columns.tsx
@@ -1,5 +1,9 @@
+import { Button } from "@/components/ui/button";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuPortal, DropdownMenuSeparator, DropdownMenuSub, DropdownMenuSubContent, DropdownMenuSubTrigger, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
 import { User } from "@/types/user"
 import { ColumnDef } from "@tanstack/react-table"
+import { MoreHorizontal } from "lucide-react";
+import { Link } from "react-router-dom";
 export const columns: ColumnDef<User>[] = [
   {
     accessorKey: "email",
@@ -14,11 +18,60 @@ export const columns: ColumnDef<User>[] = [
     header: "Số điện thoại",
   },
   {
-    accessorKey: "phoneNumber",
-    header: "Số điện thoại",
+    accessorKey: "role",
+    header: "Vai trò",
   },
   {
     accessorKey: "status",
     header: "Trạng thái",
+  },
+  {
+    id: "actions",
+    cell: ({ row }) => {
+      const product = row.original;
+
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="h-8 w-8 p-0">
+              <span className="sr-only">Open menu</span>
+              <MoreHorizontal className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>Thao tác</DropdownMenuLabel>
+            <DropdownMenuItem
+              onClick={() =>
+                navigator.clipboard.writeText(product.id.toString())
+              }
+            >
+              Sao chép ID tài khoản
+            </DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>
+              <Link to={`/product/${product.id}`}>Xem chi tiết tải khoản</Link>
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <Link to={`/product/${product.id}/edit`}>Cập nhật thông tin</Link>
+            </DropdownMenuItem>
+            <DropdownMenuSub>
+              <DropdownMenuSubTrigger>
+                Thay đổi trạng thái
+              </DropdownMenuSubTrigger>
+              <DropdownMenuPortal>
+                <DropdownMenuSubContent>
+                  <DropdownMenuItem>Kích hoạt</DropdownMenuItem>
+                  <DropdownMenuItem>Hủy kích hoạt</DropdownMenuItem>
+                </DropdownMenuSubContent>
+              </DropdownMenuPortal>
+            </DropdownMenuSub>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem className="text-red-500">
+              Xóa tài khoản
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      );
+    },
   },
 ]
