0f52b40ed625f953c7f6428575ab7cbbdc351656
huyngse <ngogiahuy775309@gmail.com>
2025-01-02 15:06:11 +0700

    refactor(type): rename file extension from tsx to ts

---
+++ src/types/order.ts
@@ -0,0 +1,23 @@
+export type Order = {
+    id: number,
+    orderId: string,
+    customerName: string,
+    orderDate: string,
+    paymentDate?: string,
+    shippingDate?: string,
+    deliveryDate?: string,
+    shippingAddress: string,
+    shippingFee: number,
+    phone: string,
+    paymentMethod: string,
+    items: {
+        id: number,
+        productName: string,
+        status: string,
+        price: number,
+        category: string,
+        image: string,
+        quantity: number,
+    }[],
+    status: "Shipped" | "Pending" | "Processing" | "Delivered" | "Cancelled" | "Returned"
+}
